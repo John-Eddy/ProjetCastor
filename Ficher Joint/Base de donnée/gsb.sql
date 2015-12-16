1c3f21ebbd386f4dc50e2b8438ed3e14e9d559cd
John-Eddy <rahamnieddy@gmail.com>
2015-12-16 13:24:03 +0100

    BDD a jour

--- Ficher Joint/Base de donnée/gsb.sql
+++ Ficher Joint/Base de donnée/gsb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 07 Décembre 2015 à 12:19
+-- Généré le :  Mer 16 Décembre 2015 à 13:22
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `etat_fiche_frais` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `etat_fiche_frais`
@@ -38,9 +38,9 @@ CREATE TABLE IF NOT EXISTS `etat_fiche_frais` (
 
 INSERT INTO `etat_fiche_frais` (`id`, `libelle`) VALUES
 (1, 'Fiche créée, saisie en cours'),
-(3, 'Saisie clôturée'),
-(4, 'Validée et mise en paiement'),
-(5, 'Remboursé');
+(2, 'Saisie clôturée'),
+(3, 'Validée et mise en paiement'),
+(4, 'Remboursé');
 
 -- --------------------------------------------------------
 
@@ -74,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `fiche_frais` (
   `idEtatFicheFrais` int(11) NOT NULL,
   `montantValide` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `dateModif` datetime NOT NULL,
-  PRIMARY KEY (`idVisiteur`)
+  PRIMARY KEY (`idVisiteur`,`mois`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -82,7 +82,12 @@ CREATE TABLE IF NOT EXISTS `fiche_frais` (
 --
 
 INSERT INTO `fiche_frais` (`idVisiteur`, `mois`, `idEtatFicheFrais`, `montantValide`, `dateModif`) VALUES
-(6, '112015', 3, '', '2015-11-26 00:00:00');
+(6, '072015', 4, '', '2015-07-30 00:00:00'),
+(6, '082015', 3, '', '2015-08-30 00:00:00'),
+(6, '092015', 3, '', '2015-09-30 00:00:00'),
+(6, '102015', 3, '', '2015-10-22 00:00:00'),
+(6, '112015', 2, '', '2015-11-26 00:00:00'),
+(6, '122015', 1, '', '2015-12-09 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -128,7 +133,8 @@ CREATE TABLE IF NOT EXISTS `ligne_frais_forfait` (
 INSERT INTO `ligne_frais_forfait` (`idVisiteur`, `mois`, `idFraisForfait`, `quantite`, `idEtatLigneFrais`) VALUES
 (6, '112015', 1, 5, 1),
 (6, '112015', 2, 7, 1),
-(6, '112015', 3, 10, 1);
+(6, '112015', 3, 10, 1),
+(6, '122015', 1, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -137,7 +143,7 @@ INSERT INTO `ligne_frais_forfait` (`idVisiteur`, `mois`, `idFraisForfait`, `quan
 --
 
 CREATE TABLE IF NOT EXISTS `ligne_frais_hors_forfait` (
-  `num` int(11) NOT NULL AUTO_INCREMENT,
+  `num` int(11) NOT NULL,
   `idVisiteur` int(11) NOT NULL,
   `mois` varchar(6) COLLATE utf8_unicode_ci NOT NULL,
   `idEtatLigneFrais` int(11) NOT NULL,
@@ -145,7 +151,7 @@ CREATE TABLE IF NOT EXISTS `ligne_frais_hors_forfait` (
   `montant` double NOT NULL,
   `libelleLigneHorsForfait` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`num`,`idVisiteur`,`mois`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `ligne_frais_hors_forfait`
@@ -167,34 +173,35 @@ CREATE TABLE IF NOT EXISTS `visiteur` (
   `prenom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `adresse` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `cp` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ville` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cp` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `ville` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `username_canonical` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email_canonical` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `enabled` tinyint(1) DEFAULT NULL,
-  `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `last_login` datetime DEFAULT NULL,
-  `locked` tinyint(1) DEFAULT NULL,
-  `expired` tinyint(1) DEFAULT NULL,
+  `locked` tinyint(1) NOT NULL,
+  `expired` tinyint(1) NOT NULL,
   `expires_at` datetime DEFAULT NULL,
   `confirmation_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password_requested_at` datetime DEFAULT NULL,
   `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
-  `credentials_expired` tinyint(1) DEFAULT NULL,
+  `credentials_expired` tinyint(1) NOT NULL,
   `credentials_expire_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_4EA587B892FC23A8` (`username_canonical`),
   UNIQUE KEY `UNIQ_4EA587B8A0D96FBF` (`email_canonical`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
 
 --
 -- Contenu de la table `visiteur`
 --
 
 INSERT INTO `visiteur` (`id`, `nom`, `prenom`, `password`, `adresse`, `cp`, `ville`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`) VALUES
-(6, 'Eddy', 'Rahmani', '$2y$13$lanyk3goolwskc44w8gooeV5.ulCa/yAzAvF.iER3db3UMgxti20.', NULL, '38260', 'Champier', 'e.rahmani', 'e.rahmani', 'abs@abs.fr', 'abs@abs.fr', 1, 'lanyk3goolwskc44w8goos4gwg4c804', '2015-12-06 22:11:59', 0, 0, NULL, NULL, NULL, 'a:0:{}', 0, NULL);
+(6, 'Rahmani', 'Eddy', '$2y$13$lanyk3goolwskc44w8gooeV5.ulCa/yAzAvF.iER3db3UMgxti20.', '', '38260', 'Champier', 'e.rahmani', 'e.rahmani', 'abs@abs.fr', 'abs@abs.fr', 1, 'lanyk3goolwskc44w8goos4gwg4c804', '2015-12-16 13:16:10', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:16:"ROLE_UTILISATEUR";}', 0, NULL),
+(7, 'Abd', 'Liza', '$2y$13$4x8cwuvbwe4goo4gcw0s8eVO8xsTOAXRBI6S.dDR6SVsbG7skASMW', NULL, '69007', 'Lyon', 'l.abd', 'l.abd', 'l.zdf@zd.fr', 'l.zdf@zd.fr', 1, '4x8cwuvbwe4goo4gcw0s8s880cs0kos', '2015-12-14 15:46:00', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:10:"ROLE_ADMIN";}', 0, NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
